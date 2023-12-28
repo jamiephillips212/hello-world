dee9027beea8f68de349685e2b0ab3c53af7da2d
Jamie Phillips <152644895+jamiephillips212@users.noreply.github.com>
2023-12-27 22:46:02 -0600

    fixed js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function rateMyPage(){
     let rating = prompt("How many stars would you rate my page? 1-5");
 
     for (let i = 0; i < rating; i++){
-        document.write("<img class=`loop-img` src=`five-star-rating-png-transparent-images-free-download-2020.png` alt=`5 pointed stars in a cartoon style` />");
+        document.write("<img class=`loop-img` src=`star.png` alt=`5 pointed star in the style of a cartoon` />");
     }
 }
 
@@ -43,51 +43,5 @@ function rateMyPage(){
     document.write("You have a lot to learn!");
 // } else {
 //  coolOrNoWarn();
-//  }
-// }
-
-// function coolorNoWarn(){
-//  const bookworm2 = prompt("Another chance to be nice! "Are the best books nonfiction books?")
-// console.log(bookworm2);
-
-// if (bookworm2 == `yes`){
-    document.write(`You're in the right place!`);
-// } else if (bookworm2 == "no"){
-// } document.write(`You have a lot to learn!`);
-// } else {
-// coolOrNoWarn();
-// }
-// }
-
-// function coolOrNoWarn(){
-//   const bookworm3 = prompt ("We can do this all day. Are the best books nonfiction books?")
-// console.log(bookworm3)
-
-// if (bookworm3 == "yes"{
-// document.write(`We agree on something. About time.`);
-// } else if (bookworm3 == "no")
-//   document.write(`Another chance! Are the best books nonfiction books?`);
-// } else {
-// coolOrNoWarn2();
-// }    
-// }
-
-
-// function coolOrNoWarn(){
-//   const bookworm2 = prompt("Another chance! Are the best books nonfiction books?")
-// console.log(bookworm3);   
-// }
-
-// if (bookworm2 =="yes"){
-// document.write(`You're in the right place!`);
-// } else if (bookworm2 == "no"){
-// document.write(`You have a lot to learn!`);
-// } else {
-// coolOrNoWarn();
-// }
-
-// function coolOrNo(){
-//    const bookworm = prompt("Are the best books nonfiction books?")
-// console.log(bookworm);
 //  }
 // }
