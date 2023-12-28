ff0f30a12b89fe0870ee61cf063893ad24797433
Jamie Phillips <152644895+jamiephillips212@users.noreply.github.com>
2023-12-27 22:24:34 -0600

    Added Loop

--- app.js
+++ app.js
@@ -19,8 +19,17 @@ function greetUser(){
 
 let response = prompt (`Question: Is the best media books?`);
 
-while (response.toLowerCase() != `yes`){
+while (response.toLowerCase)( != `yes`);{
 response = prompt(`wrong! Try again!`);
+}
+
+
+function rateMyPage(){
+    let rating = prompt("How many stars would you rate my page? 1-5");
+
+    for (let i = 0; 1 < rating; i++){
+        document.write("<img class=`loop-img` src=`five-star-rating-png-transparent-images-free-download-2020.png` alt=`5 pointed stars` />");
+    }
 }
 
 
@@ -33,6 +42,53 @@ response = prompt(`wrong! Try again!`);
 // } else if (bookworm == "no"){
     document.write("You have a lot to learn!");
 // } else {
+//  coolOrNoWarn();
+//  }
+// }
+
+// function coolorNoWarn(){
+//  const bookworm2 = prompt("Another chance to be nice! "Are the best books nonfiction books?")
+// console.log(bookworm2);
+
+// if (bookworm2 == `yes`){
+    document.write(`You're in the right place!`);
+// } else if (bookworm2 == "no"){
+// } document.write(`You have a lot to learn!`);
+// } else {
+// coolOrNoWarn();
+// }
+// }
+
+// function coolOrNoWarn(){
+//   const bookworm3 = prompt ("We can do this all day. Are the best books nonfiction books?")
+// console.log(bookworm3)
+
+// if (bookworm3 == "yes"{
+// document.write(`We agree on something. About time.`);
+// } else if (bookworm3 == "no")
+//   document.write(`Another chance! Are the best books nonfiction books?`);
+// } else {
+// coolOrNoWarn2();
+// }    
+// }
+
+
+// function coolOrNoWarn(){
+//   const bookworm2 = prompt("Another chance! Are the best books nonfiction books?")
+// console.log(bookworm3);   
+// }
+
+// if (bookworm2 =="yes"){
+// document.write(`You're in the right place!`);
+// } else if (bookworm2 == "no"){
+// document.write(`You have a lot to learn!`);
+// } else {
+// coolOrNoWarn();
+// }
+
+// function coolOrNo(){
+//    const bookworm = prompt("Are the best books nonfiction books?")
+// console.log(bookworm);
 //  coolOrNoWarn();
 //  }
 // }
